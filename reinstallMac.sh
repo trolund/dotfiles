611179367ca98b68921827f3b8f86e633fcd65c9
trolund@gmail.com <vxh77kdp>
2019-10-27 10:21:45 +0100

    update 2019

--- reinstallMac.sh
+++ reinstallMac.sh
@@ -12,11 +12,22 @@ fi
 # Update homebrew recipes
 brew update
 
+brew install git
+
+git config --global user.name "Troels Lund"
+git config --global user.email "trolund@gmail.com"
+
+brew cask install avast-security
+
 brew install python3
+
+brew install adoptopenjdk8
 
 brew cask install java8
 
 brew cask install java
+
+brew cask install docker
 
 brew cask install vlc
 
@@ -24,15 +35,53 @@ brew cask install iterm2
 
 brew cask install slack
 
-brew install git
-
-git config --global user.name "Troels Lund"
-git config --global user.email "trolund@mail.com"
-
-brew install zsh
-
-sh -c “$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+brew cask install firefox
 
 brew cask install webtorrent
 
 brew cask install brackets
+
+brew cask install google-chrome
+
+brew cask install macs-fan-control
+
+brew cask install onedrive
+
+brew cask install dotnet
+
+brew cask install dotnet-sdk
+
+brew cask install visual-studio
+
+brew cask install visual-studio-code
+
+brew cask install phpstorm
+
+brew cask install intellij-idea
+
+brew cask install webstorm
+
+brew cask install postman
+
+brew cask install transmit
+
+brew cask install private-internet-access
+
+brew cask install spectacle
+
+brew cask install sequel-pro
+
+brew cask install pycharm
+
+brew cask install betterzip
+
+brew cask install arduino
+
+brew cask install dash
+
+brew cask install go2shell
+
+brew install zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo installation done!!
