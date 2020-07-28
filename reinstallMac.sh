5a7acb78d4720a458266eb4b3f79957d1f6d9a58
Troels Lund <trolund@gmail.com>
2020-07-28 22:41:47 +0200

    Update reinstallMac.sh

--- reinstallMac.sh
+++ reinstallMac.sh
@@ -85,6 +85,8 @@ brew cask install go2shell
 
 brew install htop
 
+brew cask install private-internet-access
+
 brew install zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
